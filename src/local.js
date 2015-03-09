88c3641418278207ca860baa517368e6bed75271
Wingerted <wingerted@gmail.com>
2015-03-09 14:14:08 +0800

    change the style of code

--- src/local.js
+++ src/local.js
@@ -6,55 +6,106 @@ var SERVER = "127.0.0.1";
 var PORT = "1080";
 var KEY = "Winger";
 var REMOTE_PORT = "1080";
+var STATE_INIT = 0;
 var STATE_AUTHENTICATION = 1;
-var STATE_CONNECT = 2;
+var STATE_CONNECT_TO_SERVER = 2;
+var STATE_CONNECT_TO_HOST = 3;
+var STATE_DATA_TO_SERVER = 4;
+var STATE_DATA_TO_HOST = 5;
 
 var net = require('net');
 var buffer = require('buffer');
 
+
 var client = net.createServer(function (host_connection) {
 
-    console.log('shadowsocks client has connected by user host');
+    server_connection.on("end", function() {
+        console.log('shadowsocks client has disconnected with shadowsocks server');
+    });
 
-    host_connection.on("end", function() {console.log('shadowsocks client has disconnected with user host')});
+    server_connection.on("data", function(data) {
+        client.check_state(data);
+        //handle_data(data);
+        console.log('shadowsocks client get data from shadowsocks server')
+    });
+
+    server_connection.on("error", function() {
+        console.log('connection of shadowsocks client and server error !');
+    });
+
+    host_connection.on("end", function() {
+        client.state = STATE_INIT;
+        console.log('shadowsocks client has disconnected with user host');
+    });
 
     host_connection.on("data", function(data) {
-
         console.log('shadowsocks client get data from user host');
         client.check_state(data);
-        client.handle_data(host_connection, data);
+        handle_data(data);
         console.log(data.toString());
     });
 
-    host_connection.on("error", function() {console.log('shadowsocks client error !')});
+    host_connection.on("error", function() {
+        console.log('connection of shadowsocks client and host error !');
+    });
+
+    var handle_data = function(data) {
+        switch (client.state) {
+            case STATE_AUTHENTICATION:
+                host_connection.write(new Buffer([0x05, 0x00]));
+                break;
+            case STATE_CONNECT_TO_SERVER:
+                //server_connection.write(data);
+                break;
+            case STATE_CONNECT_TO_HOST:
+                host_connection.write(data);
+                break;
+            case STATE_DATA_TO_SERVER:
+                //server_connection.write(data);
+                break;
+            case STATE_DATA_TO_HOST:
+                host_connection.write(data);
+                break;
+            default : break;
+        }
+    };
+
 });
 
-client.state = STATE_AUTHENTICATION;
+var server_connection = net.connect(REMOTE_PORT, SERVER, function () {
+    console.log('shadowsocks server is connected');
+});
 
-client.handle_data = function(host_connection, data) {
-    switch (client.state) {
-        case STATE_CONNECT:
-            host_connection.write(new Buffer([0x05, 0x00]));
-        case STATE_CONNECT:
 
-    }
-};
-
+client.state = STATE_INIT;
 
 client.check_state = function(data) {
     switch (client.state) {
-        case STATE_AUTHENTICATION:
+        case STATE_INIT:
             if (data[0] == 5) {
-                client.state = STATE_CONNECT;
+                client.state = STATE_AUTHENTICATION;
             }
             break;
-        case STATE_CONNECT:
+        case STATE_AUTHENTICATION:
             if (data[0] == 5 && data[1] == 1 && data[2] == 0) {
-                var connect_address = data.slice(4, 7);
-                var connect_port = data.slice(8, 9);
-
+                client.state = STATE_CONNECT_TO_SERVER;
             }
             break;
+        case STATE_CONNECT_TO_SERVER:
+            if (data[0] == 5 && data[1] == 1 && data[2] == 0) {
+                client.state = STATE_CONNECT_TO_HOST;
+            }
+            break;
+        case STATE_CONNECT_TO_HOST:
+            client.state = STATE_DATA_TO_SERVER;
+            break;
+        case STATE_DATA_TO_SERVER:
+            client.state = STATE_DATA_TO_HOST;
+            break;
+        case STATE_DATA_TO_HOST:
+            //client.state = STATE_DATA_TO_SERVER;
+            break;
+        default : break;
     }
 
     console.log("Current State is " + this.state);
@@ -63,21 +114,3 @@ client.check_state = function(data) {
 client.listen(PORT, function (){
     console.log('shadowsocks client start listen');
 });
-
-
-
-
-
-var server = net.createConnection(REMOTE_PORT, SERVER, function (server_connection) {
-    console.log('shadowsocks server is connected');
-
-    server.on("end", function() {console.log('shadowsocks server has disconnected with user host')});
-
-    server.on("data", function() {console.log('shadowsocks server get data from user host')});
-
-    server.on("error", function() {console.log('shadowsocks server error !')});
-});
-
-
-
-//server.connect(REMOTE_PORT, SERVER);
